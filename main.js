80c8bf42a73b723fd9c063b071b3bebcfebf52b2
Nircek <nircek-2103@protonmail.com>
2019-01-09 20:42:34 +0100

    Add auto-resizing src: http://htmlcheats.com/html/resize-the-html5-canvas-dyamically/

--- main.js
+++ main.js
@@ -5,9 +5,21 @@ function line(x1, y1, x2, y2) {
     ctx.lineTo(x2, y2);
     ctx.stroke();
 }
+
 function start() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
+    window.addEventListener('resize', resizeCanvas, false);
+    resizeCanvas();
+}
+
+function resizeCanvas() {
+    canvas.width  = window.innerWidth;
+    canvas.height = window.innerHeight;
+    draw();
+}
+
+function draw() {
     ctx.translate(canvas.width/2, canvas.height/2);
     ctx.scale(1,-1);
     line(0,0,10,0);
