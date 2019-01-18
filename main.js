a8d379b8952bb81741c6cc651c6fd45936dfc4f3
Nircek <nircek-2103@protonmail.com>
2019-01-18 19:22:32 +0100

    Add straight()

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var canvas, ctx;
-var ax=(1+25/180)*Math.PI, ay=1/2*Math.PI, az=0;
-var jx=0.435889, jy=0.9, jz=1;
-var scale=10;
-var u=10; // Units
-var middle = {x:0, y:0};
+var A = {x:(1+25/180)*Math.PI, y:1/2*Math.PI, z:0};
+var J = {x:0.435889, y:0.9, z:1};
+var C = {x: '#FF0000', y: '#00FF00', z: '#0000FF'};
+var scale = 10;
+var M = {x:0, y:0};
 function line(p1, p2, color='black') {
     ctx.beginPath();
     ctx.moveTo(p1.x, p1.y);
@@ -23,13 +23,17 @@ function next(p, a, d) {
     return {x: p.x+d*Math.sin(a), y: p.y+d*Math.cos(a)};
 }
 
+function straight(p, a, d) {
+    line(p, next(p, A[a], J[a]*d*scale), C[a]);
+}
+
 function draw() {
     canvas.width  = window.innerWidth;
     canvas.height = window.innerHeight;
     ctx.translate(canvas.width/2, canvas.height/2);
     ctx.scale(1,-1);
-    line(middle, next(middle, ax, jx*10*scale), '#FF0000');
-    line(middle, next(middle, ay, jy*10*scale), '#00FF00');
-    line(middle, next(middle, az, jz*10*scale), '#0000FF');
+    straight(M, 'x', 10);
+    straight(M, 'y', 10);
+    straight(M, 'z', 10);
     setTimeout(draw, 200);
 }
